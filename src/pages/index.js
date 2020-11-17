74e05b47d3a4199b92b8a555030e60ed3ed7255b
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-11-18 00:22:00 +0500

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../Components/Layout"
+
+export default function Home() {
+  return (
+    <div>
+      <Layout>
+        <h1>Hello world!</h1>
+        <h3>Hi! My name is Arsalan from Home</h3>
+      </Layout>
+    </div>
+  )
+}
