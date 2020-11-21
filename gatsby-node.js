b620102697392badc9c413fa6e9b6bfe70183002
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-11-22 02:58:17 +0500

    updated files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,33 +1,66 @@
 const path = require('path')
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const result = await graphql(`
-  {
-    allContentfulProducts {
-      nodes {
-        title
-        price
-        slug
-        images {
-          fluid {
-            src
+  forProducts();
+  forProjects();
+  async function forProducts() {
+    const result = await graphql(`
+    {
+      allContentfulProducts {
+        nodes {
+          title
+          price
+          slug
+          images {
+            fluid {
+              src
+            }
           }
-        }
-        info {
-          info
+          info {
+            info
+          }
         }
       }
     }
+  `);
+    result.data.allContentfulProducts.nodes.forEach(products => {
+      createPage({
+        path: `/products/${products.slug}`,
+        component: path.resolve('./src/templates/productTemplates.js'),
+        context: {
+          products: products
+        }
+      });
+    });
+    console.log('Result ........ , ', JSON.stringify(result));
   }
-`)
-  result.data.allContentfulProducts.nodes.forEach(products => {
-    createPage({
-      path: `/products/${products.slug}`,
-      component: path.resolve('./src/templates/productTemplates.js'),
-      context: {
-        products: products
+  async function forProjects() {
+    const result = await graphql(`
+    {
+      allContentfulProjects {
+        nodes {
+          siteUrl
+          slug
+          title
+          images {
+            fluid {
+              src
+            }
+          }
+        }
       }
-    })
-  });
-  console.log('Result ........ , ', JSON.stringify(result));
+    }
+  `);
+
+    result.data.allContentfulProjects.nodes.forEach(projects => {
+      createPage({
+        path: `/projects/${projects.slug}`,
+        component: path.resolve('./src/templates/projectTemplates.js'),
+        context: {
+          projects: projects
+        }
+      });
+    });
+  }
 }
